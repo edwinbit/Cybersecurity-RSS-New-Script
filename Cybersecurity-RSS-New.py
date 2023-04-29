fd3ed307e35ba8a407f38950aee8d2031718e85e
Edwin Odeh <132147627+edwinbit@users.noreply.github.com>
2023-04-29 22:31:04 +0100

    Add files via upload

---
+++ Cybersecurity-RSS-New.py
@@ -0,0 +1,39 @@
+import feedparser
+import webbrowser
+
+
+
+def main():
+    print("--Security News Website List--")
+    print("[0]: The Hacker News")
+    print("[1]: ThreatPost")
+    print("[2]: NakedSecurity")
+    print("[3]: Naked Security Podcast")
+    print("[4]: Information Security Buzz")
+
+    website_list = ("https://feeds.feedburner.com/TheHackersNews", "https://threatpost.com/feed", "https://nakedsecurity.sophos.com/feed", "https://nakedsecurity.sophos.com/feed/", "https://feeds.feedburner.com/InformationSecurityBuzz")
+
+    website_input = int(input("Enter website by number (0-4): "))
+
+    NewsFeed = feedparser.parse(website_list[website_input])
+    article_list = []
+    article_link = []
+    for i in range(10):
+        article = NewsFeed.entries[i]
+        titles = article.title
+        link = article.link
+        article_link.append(link)
+        article_list.append(titles)
+
+    article_num = 1
+    for article in article_list:
+        print('[{}] {}'.format(str(article_num), article))
+        article_num += 1
+
+    article_link_click = False
+    while not article_link_click:
+        user_click = int(input("Choose the link you want to open (1-10): "))
+        webbrowser.open(article_link[user_click-1])
+        article_link_click = True
+
+main()
